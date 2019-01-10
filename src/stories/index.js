a787b2204d99cad02be4bc1c7a953884eec423e4
Michael Go <ggmichaelgo@gmail.com>
2019-01-10 01:31:44 -0400

    remove border lines

--- src/stories/index.js
+++ src/stories/index.js
@@ -8,15 +8,17 @@ import HistogramSlider from '../components/HistogramSlider';
 const histogramSliderStory = storiesOf('HistogramSlider', module);
 
 histogramSliderStory.add('Default', () =>
-  <HistogramSlider
-    valueList={[
-      1,1,1,1,2,2,3,3,3,4,5,6,7,8,8,8,8,8,8,
-      11,11,11,12,12,13,13,13,14,15,16,17,18,18,18,18,18,18,
-      21,21,21,22,22,23,23,23,24,25,26,27,28,28,28,28,28,28
-    ]}
-    defaultHandlePosition={[10, 50]}
-    onChange={(handlePosition) => {
-      console.log('new handle positions', handlePosition);
-    }}
-  />
+  <div style={{padding: 50}}>
+    <HistogramSlider
+      valueList={[
+        1,1,1,1,2,2,3,3,3,4,5,6,7,8,8,8,8,8,8,
+        11,11,11,12,12,13,13,13,14,15,16,17,18,18,18,18,18,18,
+        21,21,21,22,22,23,23,23,24,25,26,27,28,28,28,28,28,28
+      ]}
+      defaultHandlePosition={[10, 50]}
+      onChange={(handlePosition) => {
+        console.log('new handle positions', handlePosition);
+      }}
+    />
+  </div>
 )
